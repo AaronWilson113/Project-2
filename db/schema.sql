0e3783e39c62b517a74017c648e7d326c40d51a7
President <aaron.m.wilson@uconn.edu>
2022-08-24 18:49:18 -0400

    added in foreign key for workouts

--- db/schema.sql
+++ db/schema.sql
@@ -3,11 +3,17 @@ CREATE DATABASE fitness_db;
 
 USE fitness_db;
 
+CREATE TABLE workout (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    w_name VARCHAR (30)
+);
+
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL, 
     current_weight INT NOT NULL, 
     goal_weight INT NOT NULL, 
     target_calories INT NOT NULL, 
-    workouts VARCHAR(30) NOT NULL
+    workout_id INT,
+    CONSTRAINT fk_workout FOREIGN KEY (workout_id) REFERENCES workout(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,17 @@
 USE fitness_db;
 
-INSERT INTO user (username, current_weight, goal_weight, target_calories, workouts)
-VALUES ("Dave", 190, 180, 2000, "none"),
-       ("Greg", 240, 200, 1000, "none"),
-       ("Jessie", 140, 165, 2500, "none"),
-       ("Rachel", 190, 175, 1500, "none"),
-       ("Barb", 130, 120, 1000, "none"),
-       ("Louise", 160, 150, 2000, "none")
+
+INSERT INTO workout (w_name)
+VALUES ("Upper body"),
+       ("Lower body"),
+       ("Push"),
+       ("Pull");
+
+
+INSERT INTO user (username, current_weight, goal_weight, target_calories, workout_id)
+VALUES ("Dave", 190, 180, 2000, 001),
+       ("Greg", 240, 200, 1000, 002),
+       ("Jessie", 140, 165, 2500, 002),
+       ("Rachel", 190, 175, 1500, 003),
+       ("Barb", 130, 120, 1000, 003),
+       ("Louise", 160, 150, 2000, 002);

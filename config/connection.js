dd6d4d1e39ef91f74e2ea42a87c07a1716d62521
President <aaron.m.wilson@uconn.edu>
2022-08-25 12:00:19 -0400

    created .env file

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
+
 const sequelize = new Sequelize(
 
-    'fitness_db',
-
-    'root',
-
-    '',
+process.env.DB_NAME,
+process.env.DB_USER,
+process.env.DB_PASSWORD,
     {
         host: 'localhost',
         dialect: 'mysql',

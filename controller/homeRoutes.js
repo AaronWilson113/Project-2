3cac8b919a3a934be4fa0243b1e63e7925e5c5f4
President <aaron.m.wilson@uconn.edu>
2022-08-31 18:43:57 -0400

    modified hrefs

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -3,9 +3,19 @@ const router = require('express').Router();
 const { User, Workout } = require('../models');
 const withAuth = require('../')
 
+// route to render homepage
 router.get('/', async (req, res) => {
     try {
         res.render('homepage');
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+// route to render signup page
+router.get('/signup', async (req, res) => {
+    try {
+        res.render('signup');
     } catch(err) {
         res.status(500).json(err);
     }

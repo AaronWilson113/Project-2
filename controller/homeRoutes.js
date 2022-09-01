1be0b2a7e4cbf848f20d5655ca7e6dcd3fed8887
President <aaron.m.wilson@uconn.edu>
2022-09-01 13:57:50 -0400

    rendering homepage with logged in info

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -19,24 +19,36 @@ router.get('/', async (req, res) => {
 // route to render signup page
 router.get('/signup', async (req, res) => {
     try {
-        res.render('signup');
-
-        req.session.save(() => {
-
-            req.session.loggedIn= true
-          });
+        res.render('signup', {
+            logged_in: req.session.logged_in
+        });
     } catch(err) {
         res.status(500).json(err);
     }
 });
 
+// route to render workouts page
 router.get('/workout', async (req, res) => {
     try {
-        res.render('workouts');
+        res.render('workouts', {
+            logged_in: req.session.logged_in
+        });
 
-        req.session.save(() => {
-            req.session.loggedIn= true
-          });
+    } catch(err){
+        res.status(500).json(err)
+    }
+});
+
+// route to render home page
+router.get('/home', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id);
+
+        const user = userData.get({ plain:true })
+        res.render('homepage', {
+            ...user,
+            logged_in: req.session.logged_in
+        });
     } catch(err){
         res.status(500).json(err)
     }
